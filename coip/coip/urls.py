c108c0549e1a1691fd540428af378694df2cdd5a
gwna <38072049+geonura@users.noreply.github.com>
2023-10-25 13:50:30 +0900

    Update urls.py

--- coip/coip/urls.py
+++ coip/coip/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.main),
-    path("team/", views.team)
+    path("", views.main, name='main'),
+    path("next/", views.main2, name='main2'),
+    path("team/", views.team, name='team'),
+    path('upload/', views.upload_video, name='upload_video'),
+    path('result/<int:video_id>/', views.result_video, name='result_video'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
